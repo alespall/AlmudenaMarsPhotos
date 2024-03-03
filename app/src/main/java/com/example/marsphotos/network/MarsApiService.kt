af2a7701ef43581418312f3cc7f8545feaee1298
alespall <alespall@unirioja.es>
2024-03-04 00:17:53 +0100

    como obtener datos de internet 7

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -6,8 +6,7 @@ import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+private val retrofit = Retrofit.Builder().addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 interface MarsApiService {
