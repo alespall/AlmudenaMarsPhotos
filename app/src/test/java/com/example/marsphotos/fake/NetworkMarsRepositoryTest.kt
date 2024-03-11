5edf68b7df5e3e8a39488772e3082c269efc4845
alespall <alespall@unirioja.es>
2024-03-11 18:14:30 +0100

    agregar insercion manual de dependencias terminado

--- app/src/test/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
+++ app/src/test/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
@@ -5,11 +5,14 @@ import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
-@Test
-fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
-    runTest {
-        val repository = NetworkMarsPhotosRepository(
-            marsApiService = FakeMarsApiService()
-        )
-        assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
-    }
+class NetworkMarsRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+}
